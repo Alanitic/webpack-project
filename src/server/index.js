51111b4a2c85e88853276372748d0d0a15fc49ec
Alan Islas <alan.islas.itic@gmail.com>
2021-05-05 18:51:26 -0500

    Webpack added

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static('dist'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
 app.listen(8081, function () {
   console.log('Server running on 8081');
 });
