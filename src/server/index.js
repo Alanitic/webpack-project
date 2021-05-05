d53e2dc900d27b39839a6bdaccc749d76f6a5efa
Alan Islas <alan.islas.itic@gmail.com>
2021-05-05 18:31:04 -0500

    Cors added

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,9 @@
+var path = require('path');
 const express = require('express');
+var cors = require('cors');
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.listen(8081, function () {
