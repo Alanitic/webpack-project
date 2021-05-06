1d4907b3d75cc129f701e3933efd9b49f2fc7300
Alan Islas <alan.islas.itic@gmail.com>
2021-05-05 19:15:15 -0500

    Dev server running

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,11 @@
 module.exports = {
   entry: './src/client/index.js',
+  output: {
+    libraryTarget: 'var',
+    library: 'Client',
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: './dist',
+  },
 };
