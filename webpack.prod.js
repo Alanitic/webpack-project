81fc4f42a678e49e90f0289c55edf9347c28fa4f
Alan Islas <alan.islas.itic@gmail.com>
2021-05-06 14:38:17 -0500

    MiniCssExtractPlugin added

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
@@ -26,14 +26,6 @@ module.exports = {
       template: './src/client/views/index.html',
       filename: './index.html',
     }),
-    new CleanWebpackPlugin({
-      // Simulate the removal of files
-      dry: true,
-      // Write Logs to Console
-      verbose: true,
-      // Automatically remove all unused webpack assets on rebuild
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false,
-    }),
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
   ],
 };
