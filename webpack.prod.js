a17508a2feac39fb0a0fa8ea91f9a1a3a0ef8eba
Alan Islas <alan.islas.itic@gmail.com>
2021-05-10 16:01:40 -0500

    Service worker plugin added

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
@@ -26,6 +27,7 @@ module.exports = {
       template: './src/client/views/index.html',
       filename: './index.html',
     }),
+    new WorkboxPlugin.GenerateSW(),
     new MiniCssExtractPlugin({ filename: '[name].css' }),
   ],
 };
